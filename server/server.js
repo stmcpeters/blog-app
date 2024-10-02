f4b905fca67d7ede8b190a16e0f3ba079751c75f
stmcpeters <stmcpeters@gmail.com>
2024-10-02 13:57:43 -0700

    creates endpoint to fetch users from database

--- server/server.js
+++ server/server.js
@@ -14,6 +14,14 @@ app.get('/', (req, res) => {
   res.json('hi from your server :D');
 });
 
+app.get('/api/users', async (req, res) => {
+  try {
+    const { rows: users } = await db.query('SELECT * FROM users');
+    res.send(users);
+  } catch (error) {
+    console.error( {error} );
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`hi :D your server is on http://localhost:${PORT}`);
