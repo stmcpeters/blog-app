0c5436f048531afa393e03b6d3a1f95dc5222c77
stmcpeters <stmcpeters@gmail.com>
2024-10-02 13:59:03 -0700

    creates endpoint to fetch entries from database

--- server/server.js
+++ server/server.js
@@ -14,10 +14,21 @@ app.get('/', (req, res) => {
   res.json('hi from your server :D');
 });
 
+// fetches all user data from users table
 app.get('/api/users', async (req, res) => {
   try {
     const { rows: users } = await db.query('SELECT * FROM users');
     res.send(users);
+  } catch (error) {
+    console.error( {error} );
+  }
+})
+
+// fetches all entries data from entries table
+app.get('/api/entries', async (req, res) => {
+  try {
+    const { rows: entries } = await db.query('SELECT * FROM entries');
+    res.send(entries);
   } catch (error) {
     console.error( {error} );
   }
