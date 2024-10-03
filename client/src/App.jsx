a431143eeba9c582d115506b58c3b7a55cb68d2d
stmcpeters <stmcpeters@gmail.com>
2024-10-03 13:47:41 -0700

    moves users and entries into parent component to pass down to children

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,46 @@
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import MyNavbar from './components/NavBar'
 import ListEntries from './components/ListEntries';
-import pets from './assets/group-of-pets.png';
+import pets from '/src/assets/pets-group.webp';
 import ListUsers from './components/ListUsers';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const [entries, setEntries] = useState([]);
+
+  const loadUsers = () => {
+    fetch("http://localhost:5001/api/users")
+      .then((response) => response.json())
+      .then((users) => {
+        setUsers(users);
+      });
+  }
+
+  useEffect(() => {
+    loadUsers();
+  }, [users]);
+
+  const loadEntries = () => {
+    fetch("http://localhost:5001/api/entries")
+      .then((response) => response.json())
+      .then((entries) => {
+        setEntries(entries);
+    });
+  }
+
+  useEffect(() => {
+    loadEntries();
+  }, [entries]);
 
   return (
     <>
       <MyNavbar />
       <img className="container-div" src={pets} alt="picture of different kinds of pets" />
-      <ListEntries />
-      <ListUsers />
+      <ListEntries entries={entries}/>
+      <ListUsers users={users} />
     </>
   )
 }
